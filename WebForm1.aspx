e86ed00a934756ee2c1700912ac430ebbfdfdf4f
topsneeraj <agnihotrineeraj00@gmail.com>
2015-01-23 16:26:10 +0530

    uploaded

---
+++ WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication8.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    fname : - <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            lname : - <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+
+            city : - <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+    </div>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApplication8
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlCommand cmd;
+        SqlConnection cn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(@"Data Source=FAC\SQLEXPRESS2008;Initial Catalog=MySampleDb;Integrated Security=True");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand("insert into userinfo values(@a,@b)", cn);
+            cmd.Parameters.AddWithValue("@a", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@b", TextBox2.Text);
+        
+            cn.Open();
+          int i=  cmd.ExecuteNonQuery();
+          if (i > 0)
+          {
+              Label1.Text = "record inserted succfully";
+          }
+          else
+          {
+              Label1.Text = "sorry shktiman..!!!";
+          }
+            cn.Close();
+            Label1.Text = "record inserted succfully";
+        }
+    }
+}
